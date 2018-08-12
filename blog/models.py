f5a702ae01d0c2ecdb5f00d1a20f5b55d46ecac6
DianaSpahieva <d.s.spahieva@student.tue.nl>
2018-08-12 23:17:03 +0300

    Final changes

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.db.models import Sum
+
 
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = models.TextField()
+    text = models.IntegerField()
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
@@ -17,3 +19,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def total_sum(self):
+        total = Post.objects.all().aggregate(Sum("text"))
+        return total['text__sum']
